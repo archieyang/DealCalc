ec76541c9498b318e4798a8c542555caa153863a
Archie <yangyxcn@gmail.com>
2017-12-05 21:58:26 +0800

    Add import file button
    Signed-off-by: Archie <yangyxcn@gmail.com>

---
+++ DealCalc/EffectiveRatioChart.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using LiveCharts;
+using LiveCharts.Wpf;
+
+namespace DealCalc
+{
+    /// <summary>
+    /// EffectiveRatioChart.xaml 的交互逻辑
+    /// </summary>
+    public partial class EffectiveRatioChart : UserControl
+    {
+        public EffectiveRatioChart(List<SingleDayResult> res)
+        {
+            InitializeComponent();
+
+            SeriesCollection = new SeriesCollection();
+
+            var values = new ChartValues<double>();
+            var lables = new List<string>();
+            res.ForEach(result =>
+            {
+                values.Add(result.EffectiveRatio * 100);
+                lables.Add(result.Date.ToShortDateString());
+            });
+
+            Debug.WriteLine("resLength: " + res.Count);
+
+            SeriesCollection.Add(new ColumnSeries()
+            {
+                Values = values
+            });
+
+            Labels = lables.ToArray();
+
+            DataContext = this;
+        }
+
+        public SeriesCollection SeriesCollection { get; set; }
+        public string[] Labels { get; set; }
+        public Func<double, string> Formatter { get; set; }
+    }
+}
--- DealCalc/MainWindow.xaml.cs
+++ DealCalc/MainWindow.xaml.cs
@@ -24,13 +24,13 @@ namespace DealCalc
         public MainWindow()
         {
             InitializeComponent();
-            InitData();
+//            InitData();
         }
 
         private void InitData()
         {
+
             string path = @"C:\Users\Home\Desktop\SH#600011.txt";
-//            string path = @"C:\Users\Home\Desktop\SH#600010.txt";
             string[] text = System.IO.File.ReadAllLines(path, Encoding.Default);
 
             string line1 = text[2];
@@ -44,85 +44,48 @@ namespace DealCalc
 
             Debug.WriteLine(parts.Length);
 
-//            foreach (string s in text)
-//            {
-//                textPath.Content = s;
-//            }
-
-            Debug.WriteLine(TransactionData.CreateFromString(rawData: line1)); 
+            Debug.WriteLine(TransactionData.CreateFromString(rawData: line1));
 
             var list = text.Select(TransactionData.CreateFromString).Where(data => data != null).ToList();
 
             var process = new CoreProcessor(list);
 
-            SeriesCollection = new SeriesCollection();
-
-            var values = new ChartValues<double>();
-           
-        
-            var res = process.Process();
-            var lables = new List<string>();
-            res.ForEach(result =>
-            {
-                values.Add(result.EffectiveRatio * 100);
-                lables.Add(result.Date.ToShortDateString());
-            });
-
-            Debug.WriteLine("resLength: "+res.Count);
-
-            SeriesCollection.Add(new ColumnSeries()
-            {
-                Values = values
-            });
-
-
-            Labels = lables.ToArray();
-     
-
-            //adding series will update and animate the chart automatically
-            //            SeriesCollection.Add(new ColumnSeries
-            //            {
-            //                Title = "2016",
-            //                Values = new ChartValues<double> { 11, 56, 42 }
-            //            });
-
-            //also adding values updates and animates the chart automatically
-
-            //            Labels = new[] { "Maria", "Susan", "Charles", "Frida" };
-            //            Formatter = value => value.ToString("N");
-
             DataContext = this;
+            ContentHolder.Content = new EffectiveRatioChart(process.Process());
         }
-
-        public SeriesCollection SeriesCollection { get; set; }
-        public string[] Labels { get; set; }
-        public Func<double, string> Formatter { get; set; }
-
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            var openFileDialog = new Microsoft.Win32.OpenFileDialog();
-            openFileDialog.Multiselect = false;
-            openFileDialog.Filter = "Text files (*.txt)|*.txt";
+            var openFileDialog = new Microsoft.Win32.OpenFileDialog
+            {
+                Multiselect = false,
+                Filter = "Text files (*.txt)|*.txt"
+            };
 
             if ((bool)openFileDialog.ShowDialog())
             {
                 Debug.WriteLine(openFileDialog.FileName);
                 string[] text = System.IO.File.ReadAllLines(openFileDialog.FileName, Encoding.Default);
 
-                string line1 = text[10];
+                string line1 = text[2];
 
-                string[] parts = line1.Split(' ');
+                string[] parts = line1.Split(null);
 
                 foreach (string s in parts)
                 {
-                    System.Diagnostics.Debug.WriteLine(s);
+                    Debug.WriteLine(s);
                 }
 
-//                foreach (string s in text)
-//                {
-//                    textPath.Content = s;
-//                }
+                Debug.WriteLine(parts.Length);
+
+                Debug.WriteLine(TransactionData.CreateFromString(rawData: line1));
+
+                var list = text.Select(TransactionData.CreateFromString).Where(data => data != null).ToList();
+
+                var process = new CoreProcessor(list);
+
+                DataContext = this;
+                ContentHolder.Content = new EffectiveRatioChart(process.Process());
 
             }
 
