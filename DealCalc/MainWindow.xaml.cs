bb0d116f2fdc60135acb62ee69dfd2e0080eae82
Archie <yangyxcn@gmail.com>
2017-12-06 22:23:04 +0800

    Implement basic MVVM
    Signed-off-by: Archie <yangyxcn@gmail.com>

--- DealCalc/MainWindow.xaml.cs
+++ DealCalc/MainWindow.xaml.cs
@@ -24,71 +24,7 @@ namespace DealCalc
         public MainWindow()
         {
             InitializeComponent();
-//            InitData();
-        }
-
-        private void InitData()
-        {
-
-            string path = @"C:\Users\Home\Desktop\SH#600011.txt";
-            string[] text = System.IO.File.ReadAllLines(path, Encoding.Default);
-
-            string line1 = text[2];
-
-            string[] parts = line1.Split(null);
-
-            foreach (string s in parts)
-            {
-                Debug.WriteLine(s);
-            }
-
-            Debug.WriteLine(parts.Length);
-
-            Debug.WriteLine(TransactionData.CreateFromString(rawData: line1));
-
-            var list = text.Select(TransactionData.CreateFromString).Where(data => data != null).ToList();
-
-            var process = new CoreProcessor(list);
-
-            DataContext = this;
-            ContentHolder.Content = new EffectiveRatioChart(process.Process());
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            var openFileDialog = new Microsoft.Win32.OpenFileDialog
-            {
-                Multiselect = false,
-                Filter = "Text files (*.txt)|*.txt"
-            };
-
-            if ((bool)openFileDialog.ShowDialog())
-            {
-                Debug.WriteLine(openFileDialog.FileName);
-                string[] text = System.IO.File.ReadAllLines(openFileDialog.FileName, Encoding.Default);
-
-                string line1 = text[2];
-
-                string[] parts = line1.Split(null);
-
-                foreach (string s in parts)
-                {
-                    Debug.WriteLine(s);
-                }
-
-                Debug.WriteLine(parts.Length);
-
-                Debug.WriteLine(TransactionData.CreateFromString(rawData: line1));
-
-                var list = text.Select(TransactionData.CreateFromString).Where(data => data != null).ToList();
-
-                var process = new CoreProcessor(list);
-
-                DataContext = this;
-                ContentHolder.Content = new EffectiveRatioChart(process.Process());
-
-            }
-
+            DataContext = new RatioChartViewModel();
         }
     }
 }
---
+++ DealCalc/RatioChartViewModel.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace DealCalc
+{
+    internal class RatioChartViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private string _someText;
+        public string SomeText
+        {
+            get => _someText;
+            set
+            {
+                _someText = value;
+               OnPropertyChanged("SomeText");
+            }
+        }
+
+        private EffectiveRatioChart _chart;
+
+        public EffectiveRatioChart Chart
+        {
+            get => _chart;
+            set
+            {
+                _chart = value;
+                OnPropertyChanged("Chart");
+            }
+        }
+
+        public ICommand OpenFileCommand => new DelegateCommand(OpenFile);
+
+        private void OpenFile()
+        {
+            var openFileDialog = new Microsoft.Win32.OpenFileDialog
+            {
+                Multiselect = false,
+                Filter = "Text files (*.txt)|*.txt"
+            };
+
+            if ((bool) openFileDialog.ShowDialog())
+            {
+                SomeText = openFileDialog.FileName;
+                string[] text = System.IO.File.ReadAllLines(openFileDialog.FileName, Encoding.Default);
+
+                string line1 = text[2];
+
+                string[] parts = line1.Split(null);
+
+                foreach (string s in parts)
+                {
+                    Debug.WriteLine(s);
+                }
+
+                Debug.WriteLine(parts.Length);
+
+                Debug.WriteLine(TransactionData.CreateFromString(rawData: line1));
+
+                var list = text.Select(TransactionData.CreateFromString).Where(data => data != null).ToList();
+
+                var process = new CoreProcessor(list);
+
+                Chart = new EffectiveRatioChart(process.Process());
+            }
+        }
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
