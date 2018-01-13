6f4f8d6a5a28aeab09446c3f9076f5737a316505
Archie <yangyxcn@gmail.com>
2018-01-13 18:32:35 +0800

    Refactor SingleDayResult to independent class
    Signed-off-by: Archie <yangyxcn@gmail.com>

--- DealCalc/SingleDayProcessor.cs
+++ DealCalc/SingleDayProcessor.cs
@@ -55,26 +55,4 @@ namespace DealCalc
 
         }
     }
-
-    public class SingleDayResult
-    {
-        public readonly double EffectiveRatio;
-        public readonly double AbsEffectiveRatio;
-        public readonly DateTime Date;
-        public readonly double EffectiveAmount;
-        public readonly double TotalAmount;
-        public SingleDayResult(DateTime date, double effectiveRatio, double absEffectiveRatio, double effectiveAmount, double totalAmount)
-        {
-            Date = date;
-            EffectiveRatio = effectiveRatio;
-            AbsEffectiveRatio = absEffectiveRatio;
-            EffectiveAmount = effectiveAmount;
-            TotalAmount = totalAmount;
-        }
-
-        public override string ToString()
-        {
-            return "EffectiveRation = " + EffectiveRatio + ", EffectiveAmount=" + EffectiveAmount +", Total=" + TotalAmount;
-        }
-    }
 }
---
+++ DealCalc/SingleDayResult.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DealCalc
+{
+    public class SingleDayResult
+    {
+        public readonly double EffectiveRatio;
+        public readonly double AbsEffectiveRatio;
+        public readonly DateTime Date;
+        public readonly double EffectiveAmount;
+        public readonly double TotalAmount;
+        public SingleDayResult(DateTime date, double effectiveRatio, double absEffectiveRatio, double effectiveAmount, double totalAmount)
+        {
+            Date = date;
+            EffectiveRatio = effectiveRatio;
+            AbsEffectiveRatio = absEffectiveRatio;
+            EffectiveAmount = effectiveAmount;
+            TotalAmount = totalAmount;
+        }
+
+        public override string ToString()
+        {
+            return "Date = " + Date.ToShortDateString() + ", EffectiveAmount=" + EffectiveAmount +", Total=" + TotalAmount;
+        }
+    }
+}
